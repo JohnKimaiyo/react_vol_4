d2826a3e0fd19c0aab842be4e8f16e6a21ff9efd
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-14 22:28:26 -0800

    booklist component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from "react";
 import "./App.css";
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import Hello from "./components/Hello";
-import Title from "./components/Title";
-import MyName from "./components/MyName";
-import Form from "./components/Form";
-import Mycount from "./components/Mycount";
-import Users from "./components/Users";
-import Event from "./components/Event";
-import Range from "./components/Range";
-import Submit from "./components/Submit";
-import InputFocus from "./components/InputFocus";
+import BookList from "./components/BookList";
+
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
-      <Hello />
-      <Title name="Fronted Devloper" />
-      <Title name="Mobile Devloper" />
-      <Title name="Solidity Devloper" />
-      <MyName />
-      <Form />
-      <Mycount />
-      <Users />
-      <Event />
-      <Range/>
-      <Submit/>
-      <InputFocus/>
+    <BookList/>
     </div>
   );
 }
---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="boo-list">
+        <ul>
+          <li>The way of kings</li>
+          <li>the name of the wind</li>
+          <li>the find empire</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
