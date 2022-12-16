dd38314f71a8e34be8b5f648faf232fbc4f831be
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-16 06:57:07 -0800

    navbar conetxt api

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -5,10 +5,9 @@ export const ThemeContext = createContext();
 class ThemeContextProvider extends Component {
   state = {
     isLightTheme: true,
-    light: { syntax: "#555", ui: "ddd", bg: "#eee" },
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
     dark: { syntax: "ddd", ui: "#333", bg: "#555" },
   };
-
   render() {
     return (
       <ThemeContext.Provider value={{ ...this.state }}>
