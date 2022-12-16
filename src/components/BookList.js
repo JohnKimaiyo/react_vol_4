45f150226f971d30985ca3ebaccaa6c6c03a72f3
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-16 06:55:11 -0800

    navbar theme context api

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-
+import { ThemeContext } from "../context/ThemeContext";
 class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.content;
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="boo-list">
+      <div className="book-list">
+        style={{ color: theme.syntax, background: theme.bg }}
         <ul>
-          <li>The way of kings</li>
-          <li>the name of the wind</li>
-          <li>the find empire</li>
+          <li style={{ background: theme.ui }}>The way of kings</li>
+          <li style={{ background: theme.ui }}>the name of the wind</li>
+          <li style={{ background: theme.ui }}>the find empire</li>
         </ul>
       </div>
     );
